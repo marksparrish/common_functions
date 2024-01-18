3c2cec45be3f0e02e8fd1d09272de6c1bf094de6
Mark Parrish <mark@pipcommunications.com>
2024-01-17 17:49:02 -0800

    changed file operations function names

--- common_functions/file_operations.py
+++ common_functions/file_operations.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import pyarrow.parquet as pq
 
-def _extract(df) -> pd.DataFrame:
+def read_extract(df) -> pd.DataFrame:
     print("Extracting Data...")
     # get file names
     df = pq.read_table(source=processed_data_path).to_pandas()
     return df.reset_index(drop=True)
 
-def _load(df) -> pd.DataFrame:
+def write_load(df) -> pd.DataFrame:
     print('Loading data...')
     print("...saving processed data")
     df.to_parquet(processed_data_path, index=False, compression='gzip')
