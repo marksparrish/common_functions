6a44bde577939133548459c6e3fc3554088d30b3
Mark Parrish <mark@pipcommunications.com>
2024-02-08 10:08:58 -0800

    added database operations

---
+++ common_functions/database_operations.py
@@ -0,0 +1,65 @@
+# db_connection.py
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
+class DatabaseOperations:
+    def __init__(self, host, port, username, password, database):
+        """
+        Initialize the DatabaseConnection class with database credentials.
+
+        Parameters:
+        username (str): Database username
+        password (str): Database password
+        host (str): Database host
+        database (str): Database name
+        port (int): Database port, default is 3306
+        """
+        self.username = username
+        self.password = password
+        self.host = host
+        self.database = database
+        self.port = port
+
+        self.engine = create_engine(f'mysql+pymysql://{self.username}:{self.password}@{self.host}:{self.port}/{self.database}')
+
+    def get_engine(self):
+        """
+        Return the SQLAlchemy engine.
+
+        Returns:
+        sqlalchemy.engine.base.Engine: SQLAlchemy engine
+        """
+        return self.engine
+
+    def create_index(self, table_name, columns):
+        """
+        Create a BTREE index on the specified columns of a table.
+
+        Parameters:
+        table_name (str): Name of the table on which to create the index.
+        columns (list of str): List of column names to include in the index.
+        """
+        if not columns:
+            raise ValueError("No columns provided for index creation")
+
+        index_name = f"idx_{'_'.join(columns)}"
+        column_list = ', '.join(columns)
+        sql = text(f"CREATE INDEX `{index_name}` on `{table_name}` ({column_list}) USING BTREE")
+        # ADD INDEX `idx_precincts-2023-10-05_district_link` (`district_link`) USING BTREE;
+
+        with self.engine.connect() as connection:
+            # connection.execute(text(f"ALTER TABLE `{self.database}`.`{table_name}`"))
+            connection.execute(sql)
+
+    def create_view(self, view_name, table_name):
+        """
+        Create a view with all fields from the specified table.
+
+        Parameters:
+        view_name (str): Name of the view to be created.
+        table_name (str): Name of the table to create the view from.
+        """
+        sql_command = text(f"CREATE OR REPLACE VIEW `{view_name}` AS SELECT * FROM `{table_name}`")
+
+        with self.engine.connect() as connection:
+            connection.execute(sql_command)
